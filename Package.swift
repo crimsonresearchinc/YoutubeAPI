bdeb2ad74db893b9a0d5160f4b93817fe2b48f5c
Waqar Malik <wmalloc@users.noreply.github.com>
2023-04-28 15:54:33 -0700

    Updated requirements

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/wmalloc/YoutubeModel.git", branch: "main"),
-        .package(url: "https://github.com/wmalloc/WebService.git", .upToNextMajor(from: "0.7.0")),
+        .package(url: "https://github.com/wmalloc/WebService.git", .upToNextMajor(from: "0.7.2")),
      ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
